4b46b227be88f527519f1664ea81a90b0fa48497
Calherbe Valcin <mgenerator01@hotmail.com>
2025-04-03 19:31:24 -0400

    Node Callback Hell and Promises

--- operations.js
+++ operations.js
@@ -1,34 +1,21 @@
-const assert = require("assert").strict;
 
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
   const coll = db.collection(collection); //.collection is a method of the db object passed in
   // the collection argument passed is a string like 'campsites'
-  coll.insertOne(document,(err, result)=>{
-    assert.strictEqual(err, undefined);
-    callback(result);
-  });
+  return coll.insertOne(document);
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
   const coll = db.collection(collection);
-  coll.find().toArray((err, docs)=>{
-    assert.strictEqual(err, undefined);
-    callback(docs);
-  });
+  return coll.find().toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
   const coll = db.collection(collection);
-  coll.deleteOne(document, (err, result)=>{
-    assert.strictEqual(err, undefined);
-    callback(result);
-  });
+  return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
   const coll = db.collection(collection);
-  coll.updateOne(document, { $set: update },null, (err, result)=>{ //third argument is for configuration options that we are not using, and therefore pass null
-    assert.strictEqual(err, undefined);
-    callback(result);
-  });
+  return coll.updateOne(document, { $set: update },null);
 };
